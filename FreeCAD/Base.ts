c118ffba6ee0b9e09c8c1337ba62aec382f33814
Daniel Nylander <po@danielnylander.se>
2025-09-06 09:11:39 +0200

    Add files via upload

---
+++ FreeCAD/Base.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv-SE" sourcelanguage="en">
+  <context>
+    <name>UnitsApi</name>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="85"/>
+      <source>Standard (mm, kg, s, °)</source>
+      <translation>Standard (mm, kg, s, °)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="318"/>
+      <source>MKS (m, kg, s, °)</source>
+      <translation>MKS (m, kg, s, °)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="582"/>
+      <source>US customary (in, lb)</source>
+      <translation>US-amerikanska mått (in, lb)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="70"/>
+      <source>Imperial decimal (in, lb)</source>
+      <translation>Brittiska enheter (in/lb)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="543"/>
+      <source>Building Euro (cm, m², m³)</source>
+      <translation>Europeiska byggenheter (cm, m², m³)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="620"/>
+      <source>Building US (ft-in, sqft, cft)</source>
+      <translation>Amerikanska byggenheter (ft-in, sqft, cft)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="48"/>
+      <source>Metric small parts &amp; CNC (mm, mm/min)</source>
+      <translation>Metriska smådelar &amp; CNC (mm, mm/min)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="631"/>
+      <source>Imperial for Civil Eng (ft, ft/s)</source>
+      <translation>Brittiska enheter för civilingenjörer (fot, fot/s)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="570"/>
+      <source>FEM (mm, N, s)</source>
+      <translation>FEM (mm, N, s)</translation>
+    </message>
+    <message>
+      <location filename="../../UnitsSchemasData.h" line="57"/>
+      <source>Meter decimal (m, m², m³)</source>
+      <translation>Meter (m, m², m³)</translation>
+    </message>
+  </context>
+</TS>
