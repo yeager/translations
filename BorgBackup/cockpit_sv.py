46e95d8d5e395883018930bcd2d74dc66eac9acf
Daniel Nylander <github@danielnylander.se>
2026-09-09 21:26:06 +0000

    archive BorgBackup Swedish Cockpit translations

---
+++ BorgBackup/cockpit_sv.py
@@ -0,0 +1,18 @@
+# Swedish Cockpit translations for BorgBackup.
+SWEDISH_DICTIONARY = {
+    "**** You're welcome! ****": '**** Varsågod! ****',
+    'Files: ': 'Filer: ',
+    'Unchanged: ': 'Oförändrade: ',
+    'Modified: ': 'Ändrade: ',
+    'Added: ': 'Tillagda: ',
+    'Other: ': 'Övriga: ',
+    'Errors: ': 'Fel: ',
+    'RC: ': 'Returkod: ',
+    'Speed: ': 'Hastighet: ',
+    'Elapsed: ': 'Förfluten tid: ',
+    'RC: RUNNING': 'Returkod: KÖRS',
+    'Log': 'Logg',
+    'Quit': 'Avsluta',
+    'Toggle Translator': 'Växla Borg-språk',
+    'Cockpit for BorgBackup': 'Kontrollpanel för BorgBackup',
+}
