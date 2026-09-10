b8221d71aa9c4050f5d20f3bf7d73cb0420461af
Daniel Nylander <github@danielnylander.se>
2026-09-10 17:16:03 +0000

    l10n: archive complete OpenToonz ColorFX Swedish translation

---
+++ OpenToonz-colorfx/colorfx_sv.ts
@@ -0,0 +1,727 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv" sourcelanguage="en">
+<context>
+    <name>ArtisticSolidColor</name>
+    <message>
+        <source>Irregular</source>
+        <translation>Oregelbunden</translation>
+    </message>
+    <message>
+        <source>Horiz Offset</source>
+        <translation>Horisontell förskjutning</translation>
+    </message>
+    <message>
+        <source>Vert Offset</source>
+        <translation>Vertikal förskjutning</translation>
+    </message>
+    <message>
+        <source>Noise</source>
+        <translation>Brus</translation>
+    </message>
+</context>
+<context>
+    <name>MovingSolidColor</name>
+    <message>
+        <source>Offset</source>
+        <translation>Förskjutning</translation>
+    </message>
+    <message>
+        <source>Horiz Offset</source>
+        <translation>Horisontell förskjutning</translation>
+    </message>
+    <message>
+        <source>Vert Offset</source>
+        <translation>Vertikal förskjutning</translation>
+    </message>
+</context>
+<context>
+    <name>OutlineViewerStyle</name>
+    <message>
+        <source>OutlineViewer(OnlyDebug)</source>
+        <translation>Konturvisare (endast felsökning)</translation>
+    </message>
+    <message>
+        <source>Control Point</source>
+        <translation>Kontrollpunkt</translation>
+    </message>
+    <message>
+        <source>Center Line</source>
+        <translation>Mittlinje</translation>
+    </message>
+    <message>
+        <source>Outline Mode</source>
+        <translation>Konturläge</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+    <message>
+        <source>distance</source>
+        <translation>avstånd</translation>
+    </message>
+</context>
+<context>
+    <name>ShadowStyle</name>
+    <message>
+        <source>Hatched Shading</source>
+        <translation>Streckad skuggning</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Vinkel</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Längd</translation>
+    </message>
+</context>
+<context>
+    <name>ShadowStyle2</name>
+    <message>
+        <source>Plain Shadow</source>
+        <translation>Enkel skugga</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Vinkel</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+</context>
+<context>
+    <name>TAirbrushRasterStyle</name>
+    <message>
+        <source>Airbrush</source>
+        <translation>Färgspruta</translation>
+    </message>
+    <message>
+        <source>Blur value</source>
+        <translation>Oskärpevärde</translation>
+    </message>
+</context>
+<context>
+    <name>TBiColorStrokeStyle</name>
+    <message>
+        <source>Shade</source>
+        <translation>Skugga</translation>
+    </message>
+</context>
+<context>
+    <name>TBlendRasterStyle</name>
+    <message>
+        <source>Blend</source>
+        <translation>Blanda</translation>
+    </message>
+</context>
+<context>
+    <name>TBlendStrokeStyle2</name>
+    <message>
+        <source>Fade</source>
+        <translation>Tona</translation>
+    </message>
+    <message>
+        <source>Border Fade</source>
+        <translation>Tona kant</translation>
+    </message>
+    <message>
+        <source>Fade In</source>
+        <translation>Tona in</translation>
+    </message>
+    <message>
+        <source>Fade Out</source>
+        <translation>Tona ut</translation>
+    </message>
+</context>
+<context>
+    <name>TBraidStrokeStyle</name>
+    <message>
+        <source>Plait</source>
+        <translation>Fläta</translation>
+    </message>
+    <message>
+        <source>Twirl</source>
+        <translation>Virvel</translation>
+    </message>
+</context>
+<context>
+    <name>TBubbleStrokeStyle</name>
+    <message>
+        <source>Bubbles</source>
+        <translation>Bubblor</translation>
+    </message>
+</context>
+<context>
+    <name>TChainStrokeStyle</name>
+    <message>
+        <source>Chain</source>
+        <translation>Kedja</translation>
+    </message>
+</context>
+<context>
+    <name>TChalkFillStyle</name>
+    <message>
+        <source>Chalk</source>
+        <translation>Krita</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+    <message>
+        <source>Dot Size</source>
+        <translation>Punktstorlek</translation>
+    </message>
+</context>
+<context>
+    <name>TChalkStrokeStyle2</name>
+    <message>
+        <source>Chalk</source>
+        <translation>Krita</translation>
+    </message>
+    <message>
+        <source>Border Fade</source>
+        <translation>Tona kant</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+    <message>
+        <source>Fade In</source>
+        <translation>Tona in</translation>
+    </message>
+    <message>
+        <source>Fade Out</source>
+        <translation>Tona ut</translation>
+    </message>
+    <message>
+        <source>Noise</source>
+        <translation>Brus</translation>
+    </message>
+</context>
+<context>
+    <name>TCheckedFillStyle</name>
+    <message>
+        <source>Square</source>
+        <translation>Kvadrat</translation>
+    </message>
+    <message>
+        <source>Horiz Dist</source>
+        <translation>Horisontellt avstånd</translation>
+    </message>
+    <message>
+        <source>Horiz Angle</source>
+        <translation>Horisontell vinkel</translation>
+    </message>
+    <message>
+        <source>Vert Dist</source>
+        <translation>Vertikalt avstånd</translation>
+    </message>
+    <message>
+        <source>Vert Angle</source>
+        <translation>Vertikal vinkel</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TChessFillStyle</name>
+    <message>
+        <source>Chessboard</source>
+        <translation>Schackbräda</translation>
+    </message>
+    <message>
+        <source>Horiz Size</source>
+        <translation>Horisontell storlek</translation>
+    </message>
+    <message>
+        <source>Vert Size</source>
+        <translation>Vertikal storlek</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Vinkel</translation>
+    </message>
+</context>
+<context>
+    <name>TCircleStripeFillStyle</name>
+    <message>
+        <source>Concentric</source>
+        <translation>Koncentrisk</translation>
+    </message>
+    <message>
+        <source>X Position</source>
+        <translation>X-position</translation>
+    </message>
+    <message>
+        <source>Y Position</source>
+        <translation>Y-position</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TCrystallizeStrokeStyle</name>
+    <message>
+        <source>Tulle</source>
+        <translation>Tyll</translation>
+    </message>
+    <message>
+        <source>Crease</source>
+        <translation>Veck</translation>
+    </message>
+    <message>
+        <source>Opacity</source>
+        <translation>Opacitet</translation>
+    </message>
+</context>
+<context>
+    <name>TDottedFillStyle</name>
+    <message>
+        <source>Polka Dots</source>
+        <translation>Prickar</translation>
+    </message>
+    <message>
+        <source>Dot Size</source>
+        <translation>Punktstorlek</translation>
+    </message>
+    <message>
+        <source>Dot Distance</source>
+        <translation>Punktavstånd</translation>
+    </message>
+</context>
+<context>
+    <name>TDottedLineStrokeStyle</name>
+    <message>
+        <source>Vanishing</source>
+        <translation>Försvinnande</translation>
+    </message>
+    <message>
+        <source>Fade In</source>
+        <translation>Tona in</translation>
+    </message>
+    <message>
+        <source>Dash</source>
+        <translation>Streck</translation>
+    </message>
+    <message>
+        <source>Fade Out</source>
+        <translation>Tona ut</translation>
+    </message>
+    <message>
+        <source>Gap</source>
+        <translation>Mellanrum</translation>
+    </message>
+</context>
+<context>
+    <name>TDualColorStrokeStyle2</name>
+    <message>
+        <source>Striped</source>
+        <translation>Randig</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+</context>
+<context>
+    <name>TFriezeStrokeStyle2</name>
+    <message>
+        <source>Curl</source>
+        <translation>Lock</translation>
+    </message>
+    <message>
+        <source>Twirl</source>
+        <translation>Virvel</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TFurStrokeStyle</name>
+    <message>
+        <source>Herringbone</source>
+        <translation>Fiskben</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Vinkel</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+</context>
+<context>
+    <name>TGraphicPenStrokeStyle</name>
+    <message>
+        <source>Dashes</source>
+        <translation>Streck</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+</context>
+<context>
+    <name>TLinGradFillStyle</name>
+    <message>
+        <source>Linear Gradient</source>
+        <translation>Linjär övertoning</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Vinkel</translation>
+    </message>
+    <message>
+        <source>X Position</source>
+        <translation>X-position</translation>
+    </message>
+    <message>
+        <source>Y Position</source>
+        <translation>Y-position</translation>
+    </message>
+    <message>
+        <source>Smoothness</source>
+        <translation>Jämnhet</translation>
+    </message>
+</context>
+<context>
+    <name>TLongBlendStrokeStyle2</name>
+    <message>
+        <source>Watercolor</source>
+        <translation>Akvarell</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+</context>
+<context>
+    <name>TMatrioskaStrokeStyle</name>
+    <message>
+        <source>Toothpaste</source>
+        <translation>Tandkräm</translation>
+    </message>
+    <message>
+        <source>Stripes</source>
+        <translation>Ränder</translation>
+    </message>
+</context>
+<context>
+    <name>TMosaicFillStyle</name>
+    <message>
+        <source>Stained Glass</source>
+        <translation>Målat glas</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+    <message>
+        <source>Distortion</source>
+        <translation>Förvrängning</translation>
+    </message>
+    <message>
+        <source>Min Thick</source>
+        <translation>Min. tjocklek</translation>
+    </message>
+    <message>
+        <source>Max Thick</source>
+        <translation>Max. tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TMultiLineStrokeStyle2</name>
+    <message>
+        <source>Gouache</source>
+        <translation>Gouache</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+    <message>
+        <source>Noise</source>
+        <translation>Brus</translation>
+    </message>
+</context>
+<context>
+    <name>TNoColorRasterStyle</name>
+    <message>
+        <source>Markup</source>
+        <translation>Markering</translation>
+    </message>
+</context>
+<context>
+    <name>TNormal2StrokeStyle</name>
+    <message>
+        <source>Bump</source>
+        <translation>Bula</translation>
+    </message>
+    <message>
+        <source>Light X Pos</source>
+        <translation>Ljusets X-position</translation>
+    </message>
+    <message>
+        <source>Light Y Pos</source>
+        <translation>Ljusets Y-position</translation>
+    </message>
+    <message>
+        <source>Shininess</source>
+        <translation>Glans</translation>
+    </message>
+    <message>
+        <source>Plastic</source>
+        <translation>Plast</translation>
+    </message>
+</context>
+<context>
+    <name>TPatchFillStyle</name>
+    <message>
+        <source>Beehive</source>
+        <translation>Bikupa</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+    <message>
+        <source>Distortion</source>
+        <translation>Förvrängning</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TPointShadowFillStyle</name>
+    <message>
+        <source>Sponge Shading</source>
+        <translation>Svampskuggning</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Vinkel</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+    <message>
+        <source>Point Size</source>
+        <translation>Punktstorlek</translation>
+    </message>
+</context>
+<context>
+    <name>TRadGradFillStyle</name>
+    <message>
+        <source>Radial Gradient</source>
+        <translation>Radiell övertoning</translation>
+    </message>
+    <message>
+        <source>X Position</source>
+        <translation>X-position</translation>
+    </message>
+    <message>
+        <source>Y Position</source>
+        <translation>Y-position</translation>
+    </message>
+    <message>
+        <source>Radius</source>
+        <translation>Radie</translation>
+    </message>
+    <message>
+        <source>Smoothness</source>
+        <translation>Jämnhet</translation>
+    </message>
+</context>
+<context>
+    <name>TRopeStrokeStyle</name>
+    <message>
+        <source>Rope</source>
+        <translation>Rep</translation>
+    </message>
+    <message>
+        <source>Tilt</source>
+        <translation>Lutning</translation>
+    </message>
+</context>
+<context>
+    <name>TRubberFillStyle</name>
+    <message>
+        <source>Blob</source>
+        <translation>Klump</translation>
+    </message>
+    <message>
+        <source>Intensity</source>
+        <translation>Intensitet</translation>
+    </message>
+</context>
+<context>
+    <name>TSawToothStrokeStyle</name>
+    <message>
+        <source>Jagged</source>
+        <translation>Taggig</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+</context>
+<context>
+    <name>TSinStrokeStyle</name>
+    <message>
+        <source>Wave</source>
+        <translation>Våg</translation>
+    </message>
+    <message>
+        <source>Frequency</source>
+        <translation>Frekvens</translation>
+    </message>
+</context>
+<context>
+    <name>TSketchStrokeStyle</name>
+    <message>
+        <source>Fuzz</source>
+        <translation>Luddighet</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+</context>
+<context>
+    <name>TSprayStrokeStyle</name>
+    <message>
+        <source>Circlets</source>
+        <translation>Små cirklar</translation>
+    </message>
+    <message>
+        <source>Border Fade</source>
+        <translation>Tona kant</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+</context>
+<context>
+    <name>TStripeFillStyle</name>
+    <message>
+        <source>Banded</source>
+        <translation>Bandad</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Vinkel</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TTissueStrokeStyle</name>
+    <message>
+        <source>Gauze</source>
+        <translation>Gasväv</translation>
+    </message>
+    <message>
+        <source>Density</source>
+        <translation>Täthet</translation>
+    </message>
+    <message>
+        <source>Border Size</source>
+        <translation>Kantstorlek</translation>
+    </message>
+</context>
+<context>
+    <name>TTwirlStrokeStyle</name>
+    <message>
+        <source>Ribbon</source>
+        <translation>Band</translation>
+    </message>
+    <message>
+        <source>Twirl</source>
+        <translation>Virvel</translation>
+    </message>
+    <message>
+        <source>Shade</source>
+        <translation>Skugga</translation>
+    </message>
+</context>
+<context>
+    <name>TZigzTSinStrokeStyleagStrokeStyle</name>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TZigzagStrokeStyle</name>
+    <message>
+        <source>Zigzag</source>
+        <translation>Sicksack</translation>
+    </message>
+    <message>
+        <source>Min Distance</source>
+        <translation>Min. avstånd</translation>
+    </message>
+    <message>
+        <source>Max Distance</source>
+        <translation>Max. avstånd</translation>
+    </message>
+    <message>
+        <source>Min Angle</source>
+        <translation>Min. vinkel</translation>
+    </message>
+    <message>
+        <source>Max Angle</source>
+        <translation>Max. vinkel</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+</TS>
