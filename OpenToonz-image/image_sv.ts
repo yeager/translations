afe52413bd4f3a61d01528e534dd34ff74d7cf6b
Daniel Nylander <github@danielnylander.se>
2026-09-10 16:54:35 +0000

    l10n: archive complete OpenToonz image Swedish translation

---
+++ OpenToonz-image/image_sv.ts
@@ -0,0 +1,353 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv" sourcelanguage="en">
+<context>
+    <name>AviWriterProperties</name>
+    <message>
+        <location filename="../../image/avi/tiio_avi.cpp" line="1182"/>
+        <source>Codec</source>
+        <translation>Kodek</translation>
+    </message>
+    <message>
+        <location filename="../../image/avi/tiio_avi.cpp" line="1183"/>
+        <source>Uncompressed</source>
+        <translation>Okomprimerad</translation>
+    </message>
+</context>
+<context>
+    <name>GifWriterProperties</name>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_gif.cpp" line="257"/>
+        <source>Scale</source>
+        <translation>Skala</translation>
+    </message>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_gif.cpp" line="258"/>
+        <source>Looping</source>
+        <translation>Loopning</translation>
+    </message>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_gif.cpp" line="259"/>
+        <source>Generate Palette</source>
+        <translation>Generera palett</translation>
+    </message>
+</context>
+<context>
+    <name>Mp4WriterProperties</name>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_mp4.cpp" line="234"/>
+        <source>Quality</source>
+        <translation>Kvalitet</translation>
+    </message>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_mp4.cpp" line="235"/>
+        <source>Scale</source>
+        <translation>Skala</translation>
+    </message>
+</context>
+<context>
+    <name>PngWriterProperties</name>
+    <message>
+        <location filename="../../image/png/tiio_png.cpp" line="738"/>
+        <source>Alpha Channel</source>
+        <translation>Alfakanal</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_ffmpeg.cpp" line="177"/>
+        <source>FFmpeg timed out.
+Please check the file for errors.
+If the file doesn&apos;t play or is incomplete, 
+Please try raising the FFmpeg timeout in Preferences.</source>
+        <translation>FFmpeg fick tidsgräns.
+Kontrollera filen för fel.
+Om filen inte kan spelas upp eller är ofullständig,
+försök öka FFmpeg-tidsgränsen i Inställningar.</translation>
+    </message>
+</context>
+<context>
+    <name>SgiWriterProperties</name>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1231"/>
+        <source>Bits Per Pixel</source>
+        <translation>Bitar per bildpunkt</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1232"/>
+        <source>24 bits</source>
+        <translation>24 bitar</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1233"/>
+        <source>32 bits</source>
+        <translation>32 bitar</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1234"/>
+        <source>48 bits</source>
+        <translation>48 bitar</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1235"/>
+        <source>64 bits</source>
+        <translation>64 bitar</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1236"/>
+        <source>8 bits (Greyscale)</source>
+        <translation>8 bitar (gråskala)</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1237"/>
+        <source>Endianess</source>
+        <translation>Byteordning</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1238"/>
+        <source>Big Endian</source>
+        <translation>Big endian</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1239"/>
+        <source>Little Endian</source>
+        <translation>Little endian</translation>
+    </message>
+    <message>
+        <location filename="../../image/sgi/filesgi.cpp" line="1240"/>
+        <source>RLE-Compressed</source>
+        <translation>RLE-komprimerad</translation>
+    </message>
+</context>
+<context>
+    <name>SpriteWriterProperties</name>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="313"/>
+        <source>Top Padding</source>
+        <translation>Utfyllnad upptill</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="314"/>
+        <source>Bottom Padding</source>
+        <translation>Utfyllnad nedtill</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="315"/>
+        <source>Left Padding</source>
+        <translation>Utfyllnad till vänster</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="316"/>
+        <source>Right Padding</source>
+        <translation>Utfyllnad till höger</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="317"/>
+        <source>Scale</source>
+        <translation>Skala</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="318"/>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="319"/>
+        <source>Grid</source>
+        <translation>Rutnät</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="320"/>
+        <source>Vertical</source>
+        <translation>Vertikal</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="321"/>
+        <source>Horizontal</source>
+        <translation>Horisontell</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="322"/>
+        <source>Individual</source>
+        <translation>Individuell</translation>
+    </message>
+    <message>
+        <location filename="../../image/sprite/tiio_sprite.cpp" line="323"/>
+        <source>Trim Empty Space</source>
+        <translation>Beskär tomt utrymme</translation>
+    </message>
+</context>
+<context>
+    <name>SvgWriterProperties</name>
+    <message>
+        <location filename="../../image/svg/tiio_svg.cpp" line="1969"/>
+        <source>Stroke Mode</source>
+        <translation>Konturläge</translation>
+    </message>
+    <message>
+        <location filename="../../image/svg/tiio_svg.cpp" line="1970"/>
+        <source>Outline Quality</source>
+        <translation>Konturkvalitet</translation>
+    </message>
+    <message>
+        <location filename="../../image/svg/tiio_svg.cpp" line="1971"/>
+        <source>Centerline</source>
+        <translation>Mittlinje</translation>
+    </message>
+    <message>
+        <location filename="../../image/svg/tiio_svg.cpp" line="1972"/>
+        <source>Outline</source>
+        <translation>Kontur</translation>
+    </message>
+    <message>
+        <location filename="../../image/svg/tiio_svg.cpp" line="1973"/>
+        <source>High</source>
+        <translation>Hög</translation>
+    </message>
+    <message>
+        <location filename="../../image/svg/tiio_svg.cpp" line="1974"/>
+        <source>Medium</source>
+        <translation>Medel</translation>
+    </message>
+    <message>
+        <location filename="../../image/svg/tiio_svg.cpp" line="1975"/>
+        <source>Low</source>
+        <translation>Låg</translation>
+    </message>
+</context>
+<context>
+    <name>TgaWriterProperties</name>
+    <message>
+        <location filename="../../image/tga/tiio_tga.cpp" line="523"/>
+        <source>Bits Per Pixel</source>
+        <translation>Bitar per bildpunkt</translation>
+    </message>
+    <message>
+        <location filename="../../image/tga/tiio_tga.cpp" line="524"/>
+        <source>16 bits</source>
+        <translation>16 bitar</translation>
+    </message>
+    <message>
+        <location filename="../../image/tga/tiio_tga.cpp" line="525"/>
+        <source>24 bits</source>
+        <translation>24 bitar</translation>
+    </message>
+    <message>
+        <location filename="../../image/tga/tiio_tga.cpp" line="526"/>
+        <source>32 bits</source>
+        <translation>32 bitar</translation>
+    </message>
+    <message>
+        <location filename="../../image/tga/tiio_tga.cpp" line="527"/>
+        <source>Compression</source>
+        <translation>Komprimering</translation>
+    </message>
+</context>
+<context>
+    <name>TifWriterProperties</name>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="730"/>
+        <source>Byte Ordering</source>
+        <translation>Byteordning</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="731"/>
+        <source>Compression Type</source>
+        <translation>Komprimeringstyp</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="732"/>
+        <source>Bits Per Pixel</source>
+        <translation>Bitar per bildpunkt</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="733"/>
+        <source>24(RGB)</source>
+        <translation>24 (RGB)</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="734"/>
+        <source>48(RGB)</source>
+        <translation>48 (RGB)</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="735"/>
+        <source> 1(BW)</source>
+        <translation> 1 (svartvitt)</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="736"/>
+        <source> 8(GREYTONES)</source>
+        <translation> 8 (gråtoner)</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="737"/>
+        <source>32(RGBM)</source>
+        <translation>32 (RGBM)</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="738"/>
+        <source>64(RGBM)</source>
+        <translation>64 (RGBM)</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="739"/>
+        <source>Orientation</source>
+        <translation>Orientering</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="740"/>
+        <source>Top Left</source>
+        <translation>Överst till vänster</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="741"/>
+        <source>Top Right</source>
+        <translation>Överst till höger</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="742"/>
+        <source>Bottom Right</source>
+        <translation>Nederst till höger</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="743"/>
+        <source>Bottom Left</source>
+        <translation>Nederst till vänster</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="744"/>
+        <source>Left Top</source>
+        <translation>Vänster överst</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="745"/>
+        <source>Left Bottom</source>
+        <translation>Vänster nederst</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="746"/>
+        <source>Right Top</source>
+        <translation>Höger överst</translation>
+    </message>
+    <message>
+        <location filename="../../image/tif/tiio_tif.cpp" line="747"/>
+        <source>Right Bottom</source>
+        <translation>Höger nederst</translation>
+    </message>
+</context>
+<context>
+    <name>WebmWriterProperties</name>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_webm.cpp" line="235"/>
+        <source>Quality</source>
+        <translation>Kvalitet</translation>
+    </message>
+    <message>
+        <location filename="../../image/ffmpeg/tiio_webm.cpp" line="236"/>
+        <source>Scale</source>
+        <translation>Skala</translation>
+    </message>
+</context>
+</TS>
