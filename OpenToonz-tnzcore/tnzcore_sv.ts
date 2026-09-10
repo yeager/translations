810089b4c36ac61e2edfc21176a8646606f336af
Daniel Nylander <github@danielnylander.se>
2026-09-10 16:56:05 +0000

    l10n: archive complete OpenToonz tnzcore Swedish translation

---
+++ OpenToonz-tnzcore/tnzcore_sv.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv" sourcelanguage="en">
+<context>
+    <name>BmpWriterProperties</name>
+    <message>
+        <source>Bits Per Pixel</source>
+        <translation>Bitar per bildpunkt</translation>
+    </message>
+    <message>
+        <source>24 bits</source>
+        <translation>24 bitar</translation>
+    </message>
+    <message>
+        <source>8 bits (Greyscale)</source>
+        <translation>8 bitar (gråskala)</translation>
+    </message>
+</context>
+<context>
+    <name>JpgWriterProperties</name>
+    <message>
+        <source>Quality</source>
+        <translation>Kvalitet</translation>
+    </message>
+    <message>
+        <source>Smoothing</source>
+        <translation>Utjämning</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>colors</source>
+        <translation>färger</translation>
+    </message>
+    <message>
+        <source>Unidentified Action</source>
+        <translation>Oidentifierad åtgärd</translation>
+    </message>
+    <message>
+        <source>Skipping frame.</source>
+        <translation>Hoppar över bildruta.</translation>
+    </message>
+    <message>
+        <source>Malformed frame name</source>
+        <translation type="vanished">フレーム番号が不正な形式です</translation>
+    </message>
+</context>
+<context>
+    <name>TCenterLineStrokeStyle</name>
+    <message>
+        <source>Constant</source>
+        <translation>Konstant</translation>
+    </message>
+    <message>
+        <source>Thickness</source>
+        <translation>Tjocklek</translation>
+    </message>
+</context>
+<context>
+    <name>TRasterImagePatternStrokeStyle</name>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>Rotation</translation>
+    </message>
+</context>
+<context>
+    <name>TVectorImagePatternStrokeStyle</name>
+    <message>
+        <source>Distance</source>
+        <translation>Avstånd</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>Rotation</translation>
+    </message>
+</context>
+</TS>
