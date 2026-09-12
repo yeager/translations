19128957f6cac9f865bbecba8086b00ed98624ba
Daniel Nylander <github@danielnylander.se>
2026-09-12 08:15:30 +0200

    archive: Unit3D bot and bug Swedish translations

---
+++ Unit3D-Bot/bot-sv.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'about' => 'Handla om',
+    'bot' => 'Bot',
+    'bots' => 'bots',
+    'color' => 'Färg',
+    'command' => 'Kommando',
+    'edit-bot' => 'Redigera bot',
+    'emoji-code' => 'Emojikod',
+    'help' => 'Hjälp',
+    'icon' => 'Ikon',
+    'info' => 'Info',
+    'name' => 'namn',
+];
---
+++ Unit3D-Bug/bug-sv.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'bug-report' => 'Felrapport',
+    'bug-report-description' => 'Rapportera ett fel på webbplatsen',
+    'enter-description' => 'Beskriv problemet så bra som möjligt',
+    'enter-email' => 'Vänligen ange ditt e-postmeddelande',
+    'enter-title' => 'Vänligen välj en riktig titel',
+    'enter-username' => 'Vänligen ange ditt användarnamn',
+    'high' => 'Hög',
+    'low' => 'Låg',
+    'priority' => 'Prioritet',
+    'priority-description' => 'Välj bara mycket högt om felet verkligen är ett problem för att använda webbplatsen.',
+    'very-high' => 'Mycket hög',
+];
