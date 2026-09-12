11bc83ce9cac90309614835ed1f176244c078d0a
Daniel Nylander <github@danielnylander.se>
2026-09-12 08:17:20 +0200

    archive: Unit3D graveyard Swedish translation

---
+++ Unit3D-Graveyard/graveyard-sv.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'dead' => 'Död',
+    'graveyard' => 'Kyrkogård',
+    'guidelines' => 'Riktlinjer',
+    'guidelines-content' => '1) Du kan inte återuppliva dina egna uppladdningar. <br> 2) Låt inte uppstå något du inte heller kan begå.',
+    'howto' => 'Heres Regeln',
+    'howto-desc1' => 'Du måste frö <span class=\"text-green\"><em>: namn</em></span> i <span class="text-red text-bold">30 dagar</span> för en framgångsrik resurction. I vilket fall när din nuvarande utsättningstid av',
+    'howto-desc2' => 'Du kommer att bli belönad',
+    'howto-hits' => 'träffar',
+    'pending' => 'Avvaktan',
+    'resurrect' => 'Återuppväcka',
+    'reward' => 'Freeleech tokens',
+    'current-seedtime' => 'Aktuell seedningstid',
+    'not-rewarded' => 'Ingen belöning',
+    'resurrect-canceled' => 'Återupplivningen har avbrutits!',
+    'resurrect-complete' => 'Torrentåterupplivningen är klar! Du belönas automatiskt när kraven på seedningstid är uppfyllda.',
+    'resurrect-date' => 'Datum för återupplivning',
+    'resurrect-failed-own' => 'Torrentåterupplivningen misslyckades! Du kan inte återuppliva dina egna uppladdningar.',
+    'resurrect-failed-pending' => 'Torrentåterupplivningen misslyckades! Den här torrenten väntar redan på återupplivning.',
+    'rewarded' => 'Belönad',
+    'seedtime-goal' => 'Mål för seedningstid',
+    'howto-desc' => 'Du måste seeda den här torrenten i 30 dagar för en lyckad återupplivning. När din aktuella seedningstid på :currentSeedtime når :requiredSeedtime belönas du med :tokens freeleech-token.',
+];
