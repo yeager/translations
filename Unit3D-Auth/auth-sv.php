fba2b32b2e17146660ff8dea20689e4e0b3db6e1
Daniel Nylander <github@danielnylander.se>
2026-09-12 08:12:39 +0200

    archive: Unit3D auth Swedish translation

---
+++ Unit3D-Auth/auth-sv.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => 'Dessa uppgifter stämmer inte överens med vårt register.',
+    'throttle' => 'För många inloggningsförsök. Var vänlig försök igen om :seconds sekunder.',
+    'activation-error' => 'Förbjuden eller ogiltig token, eller kontot är redan bekräftat!',
+    'activation-success' => 'Kontot är bekräftat! Du kan nu logga in!',
+    'add-image' => 'Lägg till ytterligare en bildlänk',
+    'add-profile' => 'Lägg till ytterligare en profillänk',
+    'allow-invite-appl' => 'Öppen registrering är stängd! Du måste bli inbjuden för att registrera dig. Ansökningar om medlemskap är dock öppna. Du har omdirigerats till ansökningssidan!',
+    'allow-invite' => 'Öppen registrering är stängd! Du måste bli inbjuden för att registrera dig. Du har omdirigerats till inloggningssidan!',
+    'are-you' => 'Är du:',
+    'application' => 'Ansökan',
+    'apply' => 'Ansök',
+    'appl-closed' => 'Ansökningar är stängda',
+    'appl-intro' => 'är en sluten community. Du måste ha en inbjudningslänk för att registrera dig. Om du inte kan få en inbjudan kan du fylla i följande medlemsansökan.',
+    'appl-reason' => 'Hur fick du höra talas om ”:sitename” och varför vill du gå med? ',
+    'application-submitted' => 'Din ansökan har skickats. Du får snart ett e-postmeddelande!',
+    'banned' => 'Det här kontot är avstängt!',
+    'check-later' => 'Kom tillbaka senare!',
+    'code' => 'TOTP-kod',
+    'confirm-password' => 'Bekräfta lösenord',
+    'confirm-new-password' => 'Bekräfta nytt lösenord',
+    'delete-image' => 'Ta bort senaste bildlänken',
+    'delete-profile' => 'Ta bort senaste profillänken',
+    'email' => 'E-post',
+    'email-verification-link' => 'En länk för e-postverifiering har skickats till dig!',
+    'enter-recovery' => 'Ange en av återställningskoderna som du fick när du först aktiverade tvåfaktorsautentisering.',
+    'enter-totp' => 'Ange den sexsiffriga autentiseringskod som genereras av din app.',
+    'invalid-key' => 'Ogiltig eller utgången inbjudningsnyckel!',
+    'login' => 'Logga in',
+    'login-now-on' => 'Logga in nu på',
+    'logout' => 'Logga ut',
+    'lost-password' => 'Har du glömt lösenordet?',
+    'lost-username' => 'Har du glömt användarnamnet?',
+    'need-invite' => 'Öppen registrering är inaktiverad och du behöver en inbjudan för att få åtkomst!',
+    'new-password' => 'Nytt lösenord',
+    'newbie' => 'Ny i spelet',
+    'not-a-member' => 'Inte medlem ännu? Registrera dig på mindre än 30 sekunder.',
+    'not-activated' => 'Det här kontot har inte aktiverats och finns fortfarande i valideringsgruppen. Kontrollera din e-post efter aktiveringslänken. Om du inte har fått aktiveringskoden klickar du på ”Har du glömt lösenordet?” och följer stegen.',
+    'password' => 'Lösenord',
+    'password-confirm-desc' => 'Bekräfta lösenordet innan du fortsätter.',
+    'password-confirmation' => 'Lösenordsbekräftelse',
+    'password-reset' => 'Återställ lösenord',
+    'proof-image' => 'Länk till skärmbild av profilsidan',
+    'proof-image-title' => 'Länkar till skärmbilder av profilsidor',
+    'proof-profile' => 'Länk till profilsidan',
+    'proof-profile-title' => 'Länkar till dina profiler',
+    'proof-min' => '(minst 2, rekommenderat 3)',
+    'recover-my-password' => 'Återställ mitt lösenord',
+    'recovery-code' => 'Återställningskod',
+    'register-thanks' => 'Tack för att du registrerade dig! Kontrollera din e-post för att bekräfta kontot.',
+    'remember-me' => 'Kom ihåg mig',
+    'require-rules' => 'Läs och godkänn våra regler genom att rulla längst ned på sidan.',
+    'send-verification-email' => 'Skicka e-post för verifiering',
+    'signup' => 'Registrera dig',
+    'totp-code' => 'TOTP-kod',
+    'unlock' => 'Lås upp',
+    'user-icon' => 'Användarikon',
+    'username' => 'Användarnamn',
+    'verify' => 'Verifiera',
+    'verify-email' => 'Verifiera e-post',
+    'verifying' => 'Verifierar …',
+    'veteran' => 'Erfarenhet av privata trackers',
+    'welcome' => 'Välkommen tillbaka!',
+    'welcome-restore' => 'Välkommen tillbaka! Ditt konto är inte längre inaktiverat!',
+    'two-factor-confirm-desc' => 'Bekräfta tvåfaktorskoden innan du fortsätter.',
+    'two-factor-confirmation' => 'Bekräftelse av tvåfaktorsautentisering',
+];
