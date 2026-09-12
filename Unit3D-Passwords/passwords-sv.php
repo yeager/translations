a59345a550fe1d87e2408276dd35ef7570a9c6f0
Daniel Nylander <github@danielnylander.se>
2026-09-12 08:08:23 +0200

    archive: Unit3D passwords Swedish translation

---
+++ Unit3D-Passwords/passwords-sv.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Lösenord måste innehålla minst sex tecken och matcha varandra.',
+    'reset' => 'Lösenordet har blivit återställt!',
+    'sent' => 'Klart: Om e-postadressen matchar ett konto skickas en länk för lösenordsåterställning.',
+    'token' => 'Koden för lösenordsåterställning är ogiltig.',
+    'user' => 'Det finns ingen användare med den e-postadressen.',
+];
