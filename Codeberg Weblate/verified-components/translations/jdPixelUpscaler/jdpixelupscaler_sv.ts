ff101edae303b357c4ed4fa3ba490c454802ed50
Daniel Nylander <1206564+yeager@users.noreply.github.com>
2026-09-13 19:08:58 +0200

    Complete jdPixelUpscaler Swedish translation

---
+++ Codeberg Weblate/verified-components/translations/jdPixelUpscaler/jdpixelupscaler_sv.ts
@@ -0,0 +1,210 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../ui/AboutDialog.ui" line="14"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutDialog.ui" line="40"/>
+        <source>Upscale Images per Pixel</source>
+        <translation>Förstora bilder pixel för pixel</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutDialog.ui" line="50"/>
+        <source>This Program is licensed under GPL 3</source>
+        <translation>Programmet är licensierat under GPL 3</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutDialog.ui" line="70"/>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="77"/>
+        <source>Open</source>
+        <translation>Öppna</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="36"/>
+        <source>Source Image</source>
+        <translation>Källbild</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="21"/>
+        <source>jdPixelUpscaler upscales images by simply replacing one pixel with several of the same color. For example, if you set the scaling factor to 2, one pixel becomes 2x2 pixels of the same color. No attention is paid to the surrounding pixels or to transitions. For certain graphics (mainly pixel art or similar in very low resolution), this may give better results than other methods.</source>
+        <translation>jdPixelUpscaler förstorar bilder genom att helt enkelt ersätta en pixel med flera pixlar i samma färg. Om du till exempel anger skalningsfaktorn 2 blir en pixel 2x2 pixlar i samma färg. Ingen hänsyn tas till omgivande pixlar eller övergångar. För vissa typer av grafik, främst pixelgrafik eller liknande med mycket låg upplösning, kan detta ge bättre resultat än andra metoder.</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="64"/>
+        <location filename="../ui/MainWindow.ui" line="128"/>
+        <source>Open an image to get started</source>
+        <translation>Öppna en bild för att börja</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="97"/>
+        <source>Scaled Image</source>
+        <translation>Skalad bild</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="141"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="178"/>
+        <source>Scaling factor:</source>
+        <translation>Skalningsfaktor:</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="217"/>
+        <source>File</source>
+        <translation>Arkiv</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="243"/>
+        <source>Exit</source>
+        <translation>Avsluta</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="252"/>
+        <source>View Source</source>
+        <translation>Visa källkod</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="261"/>
+        <source>Report Bug</source>
+        <translation>Rapportera fel</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="270"/>
+        <source>Translate</source>
+        <translation>Översätt</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="279"/>
+        <source>Donate</source>
+        <translation>Donera</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="288"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="297"/>
+        <source>About Qt</source>
+        <translation>Om Qt</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="43"/>
+        <source>Image files</source>
+        <translation>Bildfiler</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="45"/>
+        <source>PNG files</source>
+        <translation>PNG-filer</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="47"/>
+        <source>JPEG files</source>
+        <translation>JPEG-filer</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="49"/>
+        <source>All files</source>
+        <translation>Alla filer</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="65"/>
+        <location filename="../MainWindow.cpp" line="81"/>
+        <source>Size: {{Size}}</source>
+        <translation>Storlek: {{Size}}</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="115"/>
+        <source>Could not load Image</source>
+        <translation>Kunde inte läsa in bild</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="115"/>
+        <source>Could not load Image {{Path}}</source>
+        <translation>Kunde inte läsa in bilden {{Path}}</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="121"/>
+        <source>Error</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="121"/>
+        <source>There is no scaled image</source>
+        <translation>Det finns ingen skalad bild</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="132"/>
+        <source>Could not save Image</source>
+        <translation>Kunde inte spara bild</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="132"/>
+        <source>Could not save Image {{Path}}</source>
+        <translation>Kunde inte spara bilden {{Path}}</translation>
+    </message>
+</context>
+<context>
+    <name>jdPixelUpscaler</name>
+    <message>
+        <location filename="../main.cpp" line="33"/>
+        <source>Set the scaling factor</source>
+        <translation>Ange skalningsfaktorn</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="34"/>
+        <source>Writes the file and exit</source>
+        <translation>Skriver filen och avslutar</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="35"/>
+        <source>Loads an Image</source>
+        <translation>Läser in en bild</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="47"/>
+        <source>&quot;{{Path}}&quot; does not exists</source>
+        <translation>”{{Path}}” finns inte</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>&quot;{{Path}}&quot; is not a file</source>
+        <translation>”{{Path}}” är inte en fil</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="59"/>
+        <source>Could not load Image &quot;{{Path}}&quot;</source>
+        <translation>Kunde inte läsa in bilden ”{{Path}}”</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>&quot;{{ScaleFactor}}&quot; is not an Integer</source>
+        <translation>”{{ScaleFactor}}” är inte ett heltal</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="78"/>
+        <source>You need to load an Image when using this Argument</source>
+        <translation>Du måste läsa in en bild när du använder detta argument</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="85"/>
+        <source>Could not write to &quot;{{Path}}&quot;</source>
+        <translation>Kunde inte skriva till ”{{Path}}”</translation>
+    </message>
+</context>
+</TS>
