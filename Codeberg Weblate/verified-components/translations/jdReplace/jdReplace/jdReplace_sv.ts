5734cafdcec2be307b8e70f7fa5fe5bcc9ef0dab
Daniel Nylander <1206564+yeager@users.noreply.github.com>
2026-09-13 12:16:24 +0200

    Mirror jdReplace Swedish translation

---
+++ Codeberg Weblate/verified-components/translations/jdReplace/jdReplace/jdReplace_sv.ts
@@ -0,0 +1,136 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="../__init__.py" line="28"/>
+        <source>With this program you can search and replace a text in all files of a folder</source>
+        <translation>Med det här programmet kan du söka efter och ersätta text i alla filer i en katalog</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="30"/>
+        <source>This program is licensed under GNU GPL 3</source>
+        <translation>Programmet är licensierat under GNU GPL 3</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="31"/>
+        <source>View Source</source>
+        <translation>Visa källkod</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="32"/>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="69"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>ReplaceThread</name>
+    <message>
+        <location filename="../__init__.py" line="95"/>
+        <source>Searching {{path}}...</source>
+        <translation>Söker i {{path}} …</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="137"/>
+        <source>Could not replace text in {{path}}. Maybe it&apos;s a binary file.</source>
+        <translation>Kunde inte ersätta text i {{path}}. Filen kan vara binär.</translation>
+    </message>
+</context>
+<context>
+    <name>StartWindow</name>
+    <message>
+        <location filename="../__init__.py" line="151"/>
+        <source>Directory:</source>
+        <translation>Katalog:</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="217"/>
+        <location filename="../__init__.py" line="153"/>
+        <source>Browse</source>
+        <translation>Bläddra</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="154"/>
+        <source>Search for:</source>
+        <translation>Sök efter:</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="156"/>
+        <source>Replace with:</source>
+        <translation>Ersätt med:</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="158"/>
+        <source>Search Subdirectories</source>
+        <translation>Sök i underkataloger</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="159"/>
+        <source>Skip Hidden</source>
+        <translation>Hoppa över dolda filer</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="160"/>
+        <source>Follow Symlinks</source>
+        <translation>Följ symboliska länkar</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="162"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="237"/>
+        <location filename="../__init__.py" line="163"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="241"/>
+        <location filename="../__init__.py" line="240"/>
+        <source>Finished</source>
+        <translation>Slutförd</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="241"/>
+        <source>The text has been successfully replaced in all files</source>
+        <translation>Texten har ersatts i alla filer.</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="243"/>
+        <source>Canceled</source>
+        <translation>Avbruten</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="254"/>
+        <source>Not a directory</source>
+        <translation>Inte en katalog</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="254"/>
+        <source>&apos;{{path}}&apos; is not a directory!</source>
+        <translation>”{{path}}” är inte en katalog!</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="259"/>
+        <source>No search text</source>
+        <translation>Ingen söktext</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="259"/>
+        <source>Please enter a text to search for</source>
+        <translation>Ange text att söka efter</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="263"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+</context>
+</TS>
