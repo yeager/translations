e0905a395b3ee2b8b2e60eeaab3e36af72491001
Daniel Nylander <1206564+yeager@users.noreply.github.com>
2026-09-13 19:29:48 +0200

    Add reviewed Swedish qdmr translations

---
+++ Codeberg Weblate/verified-components/qdmr/glossary.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE martif PUBLIC "ISO 12200:1999A//DTD MARTIF core (DXFcdV04)//EN" "TBXcdv04.dtd">
+<martif type="TBX" xml:lang="en">
+    <martifHeader>
+        <fileDesc>
+            <sourceDesc>
+                <p>Translate Toolkit</p>
+            </sourceDesc>
+        </fileDesc>
+    </martifHeader>
+    <text>
+        <body>
+            <termEntry>
+                <langSet xml:lang="en"><tig><term>TS 1</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>TS 1</term></tig></langSet>
+                <descrip>TS 1/2 means time-slot 1/2, respectively. As it is a technical term, it might be better to stick to this abbreviation.</descrip>
+            </termEntry>
+            <termEntry id="SettingsDialogUpdate Device Clock">
+                <langSet xml:lang="en"><tig><term>Update Device Clock</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>Update Device Clock</term></tig></langSet>
+                <descrip>An option within the settings dealog. If enabled, the device clock is set to the systems time.</descrip>
+            </termEntry>
+            <termEntry id="APRSSystemDialogEdit APRS system">
+                <langSet xml:lang="en"><tig><term>APRS</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>APRS</term></tig></langSet>
+                <descrip>Stands for Automatic Packet Repoting System. Usually used to broadcast the own position to a network.</descrip>
+            </termEntry>
+            <termEntry id="SatelliteTransponderDialogBeacon">
+                <langSet xml:lang="en"><tig><term>Beacon</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>Beacon</term></tig></langSet>
+                <descrip>Describes a radio beacon of a satellite that usually transmits telemetry or some identification constantly.</descrip>
+            </termEntry>
+            <termEntry id="PropertyDelegateNone">
+                <langSet xml:lang="en"><tig><term>None</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>None</term></tig></langSet>
+                <descrip>This is an option for a property of type *Level*. It has 12 possible values: Off, 1, ..., 10, None. *None* represents an invalid level value.</descrip>
+            </termEntry>
+            <termEntry id="PropertyDelegateOff">
+                <langSet xml:lang="en"><tig><term>Off</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>Off</term></tig></langSet>
+                <descrip>Possible value of a property with type *Level*. Possible values are Off, 1, ..., 10, None. This value represents a level, that disables a feature.</descrip>
+            </termEntry>
+            <termEntry id="TransponderFrequencyEditorNone">
+                <langSet xml:lang="en"><tig><term>None</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>None</term></tig></langSet>
+                <descrip>Reprents a missing frequency for a transponder or beacon.</descrip>
+            </termEntry>
+            <termEntry id="DMRContactDialogExtensions">
+                <langSet xml:lang="en"><tig><term>Extensions</term></tig></langSet>
+                <langSet xml:lang="sv"><tig><term>Extensions</term></tig></langSet>
+                <descrip>Tab header showing all contact extensions.</descrip>
+            </termEntry>
+        </body>
+    </text>
+</martif>
