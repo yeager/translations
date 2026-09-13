4d87986cf9717fc16497bd6dbc652e38db9327be
Daniel Nylander <1206564+yeager@users.noreply.github.com>
2026-09-13 12:17:54 +0200

    Mirror jdNBTExplorer Swedish translation

---
+++ Codeberg Weblate/verified-components/translations/jdNBTExplorer/jdNBTExplorer/jdNBTExplorer_sv.ts
@@ -0,0 +1,435 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <source>An editor for Minecraft NBT files</source>
+        <translation>En redigerare för Minecraft NBT-filer</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <source>This Program is licensed under GPL 3</source>
+        <translation>Programmet är licensierat under GPL 3</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <source>Translators</source>
+        <translation>Översättare</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <source>The following people translated jdNBTExplorer:</source>
+        <translation>Följande personer har översatt jdNBTExplorer:</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <source>Changelog</source>
+        <translation>Ändringslogg</translation>
+    </message>
+    <message>
+        <location filename="../ui/AboutWindow.ui" line="0"/>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+</context>
+<context>
+    <name>EditWindow</name>
+    <message>
+        <location filename="../EditWindow.py" line="56"/>
+        <source>New</source>
+        <translation>Ny</translation>
+    </message>
+    <message>
+        <location filename="../EditWindow.py" line="62"/>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <location filename="../EditWindow.py" line="85"/>
+        <source>Name can&apos;t be empty</source>
+        <translation>Namnet får inte vara tomt</translation>
+    </message>
+    <message>
+        <location filename="../EditWindow.py" line="85"/>
+        <source>You need to set a Name</source>
+        <translation>Du måste ange ett namn</translation>
+    </message>
+    <message>
+        <location filename="../EditWindow.py" line="109"/>
+        <location filename="../EditWindow.py" line="99"/>
+        <location filename="../EditWindow.py" line="93"/>
+        <source>Invalid Value</source>
+        <translation>Ogiltigt värde</translation>
+    </message>
+    <message>
+        <location filename="../EditWindow.py" line="109"/>
+        <location filename="../EditWindow.py" line="99"/>
+        <location filename="../EditWindow.py" line="93"/>
+        <source>This value is not allowed for this type</source>
+        <translation>Det här värdet är inte tillåtet för den här typen</translation>
+    </message>
+    <message>
+        <location filename="../ui/EditWindow.ui" line="0"/>
+        <source>Name:</source>
+        <translation>Namn:</translation>
+    </message>
+    <message>
+        <location filename="../ui/EditWindow.ui" line="0"/>
+        <source>Value:</source>
+        <translation>Värde:</translation>
+    </message>
+    <message>
+        <location filename="../ui/EditWindow.ui" line="0"/>
+        <source>Type:</source>
+        <translation>Typ:</translation>
+    </message>
+</context>
+<context>
+    <name>Language</name>
+    <message>
+        <location filename="../Languages.py" line="6"/>
+        <source>English</source>
+        <translation>Engelska</translation>
+    </message>
+    <message>
+        <location filename="../Languages.py" line="7"/>
+        <source>German</source>
+        <translation>Tyska</translation>
+    </message>
+    <message>
+        <location filename="../Languages.py" line="8"/>
+        <source>Dutch</source>
+        <translation>Nederländska</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.py" line="99"/>
+        <location filename="../MainWindow.py" line="64"/>
+        <source>NBT files</source>
+        <translation>NBT-filer</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="100"/>
+        <location filename="../MainWindow.py" line="65"/>
+        <source>All files</source>
+        <translation>Alla filer</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="74"/>
+        <source>File not found</source>
+        <translation>Filen hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="74"/>
+        <source>{{path}} was not found</source>
+        <translation>{{path}} hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="93"/>
+        <source>Not an NBT File</source>
+        <translation>Inte en NBT-fil</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="93"/>
+        <source>This File does not look like an NBT File</source>
+        <translation>Filen verkar inte vara en NBT-fil</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="122"/>
+        <source>No recent files</source>
+        <translation>Inga senaste filer</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="133"/>
+        <source>Clear</source>
+        <translation>Rensa</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="187"/>
+        <source>File modified</source>
+        <translation>Filen har ändrats</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="187"/>
+        <source>The File has been modified. Do you want to save your changes?</source>
+        <translation>Filen har ändrats. Vill du spara ändringarna?</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="202"/>
+        <source>Show this message on startup</source>
+        <translation>Visa det här meddelandet vid start</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="205"/>
+        <source>Warning</source>
+        <translation>Varning</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="206"/>
+        <source>jdNBTExplorer is a programme that allows you to edit Minecraft&apos;s NBT files. Using it without proper knowledge can destroy your world. Therefore, please always make a backup of your world before using it.</source>
+        <translation>jdNBTExplorer är ett program som låter dig redigera Minecrafts NBT-filer. Felaktig användning kan förstöra din värld. Säkerhetskopiera alltid världen innan du använder programmet.</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>File</source>
+        <translation>Fil</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Recent Files</source>
+        <translation>Senaste filer</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Tag</source>
+        <translation>Tagg</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>New</source>
+        <translation>Ny</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Open</source>
+        <translation>Öppna</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Open Directory</source>
+        <translation>Öppna katalog</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Exit</source>
+        <translation>Avsluta</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>New Tag</source>
+        <translation>Ny tagg</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Edit Tag</source>
+        <translation>Redigera tagg</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>New Compound</source>
+        <translation>Ny sammansättning</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>New List</source>
+        <translation>Ny lista</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Show Welcome Dialog</source>
+        <translation>Visa välkomstdialog</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>About Qt</source>
+        <translation>Om Qt</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>View Source</source>
+        <translation>Visa källkod</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Report Bug</source>
+        <translation>Rapportera fel</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Translate</source>
+        <translation>Översätt</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Donate</source>
+        <translation>Donera</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDirectoryTypeDialog</name>
+    <message>
+        <location filename="../ui/OpenDirectoryTypeDialog.ui" line="0"/>
+        <source>Open Directory</source>
+        <translation>Öppna katalog</translation>
+    </message>
+    <message>
+        <location filename="../ui/OpenDirectoryTypeDialog.ui" line="0"/>
+        <source>Which files would you like to open?</source>
+        <translation>Vilka filer vill du öppna?</translation>
+    </message>
+    <message>
+        <location filename="../ui/OpenDirectoryTypeDialog.ui" line="0"/>
+        <source>Only NBT files</source>
+        <translation>Endast NBT-filer</translation>
+    </message>
+    <message>
+        <location filename="../ui/OpenDirectoryTypeDialog.ui" line="0"/>
+        <source>Only Region files</source>
+        <translation>Endast regionfiler</translation>
+    </message>
+    <message>
+        <location filename="../ui/OpenDirectoryTypeDialog.ui" line="0"/>
+        <source>All files</source>
+        <translation>Alla filer</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="../SettingsWindow.py" line="25"/>
+        <source>System language</source>
+        <translation>Systemspråk</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>Language:</source>
+        <translation>Språk:</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>(needs Restart)</source>
+        <translation>(kräver omstart)</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>Length of recent files list:</source>
+        <translation>Längd på listan över senaste filer:</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>Ask before closing unsaved Files</source>
+        <translation>Fråga före stängning av osparade filer</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>Show welcome message on startup</source>
+        <translation>Visa välkomstmeddelande vid start</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>Reset</source>
+        <translation>Återställ</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsWindow.ui" line="0"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+</context>
+<context>
+    <name>TreeWidget</name>
+    <message>
+        <location filename="../TreeWidget.py" line="27"/>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="27"/>
+        <source>Value</source>
+        <translation>Värde</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="27"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="107"/>
+        <location filename="../TreeWidget.py" line="78"/>
+        <source>Can&apos;t read file</source>
+        <translation>Kan inte läsa filen</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="107"/>
+        <location filename="../TreeWidget.py" line="78"/>
+        <source>Can&apos;t read {{path}}. Maybe it&apos;s not a NBT File.</source>
+        <translation>Kan inte läsa {{path}}. Filen kanske inte är en NBT-fil.</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="267"/>
+        <source>New Compound</source>
+        <translation>Ny sammansättning</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="267"/>
+        <source>Please enter a name for the new compound</source>
+        <translation>Ange ett namn för den nya sammansättningen</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="282"/>
+        <source>New List</source>
+        <translation>Ny lista</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="282"/>
+        <source>Please enter a name for the new List</source>
+        <translation>Ange ett namn för den nya listan</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="297"/>
+        <source>Rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <location filename="../TreeWidget.py" line="297"/>
+        <source>Please enter a new Name</source>
+        <translation>Ange ett nytt namn</translation>
+    </message>
+</context>
+</TS>
