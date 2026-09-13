c1d4e44fd36f4599719431debd4d10ea823d7126
Daniel Nylander <1206564+yeager@users.noreply.github.com>
2026-09-13 12:12:18 +0200

    Complete jdSimpleAutostart Swedish translation

---
+++ Codeberg Weblate/verified-components/translations/jdSimpleAutostart/jdSimpleAutostart/jdSimpleAutostart_sv.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AddEditDialog</name>
+    <message>
+        <location filename="../AddEditDialog.py" line="34"/>
+        <source>Add new autostart entry</source>
+        <translation>Lägg till ny autostartpost</translation>
+    </message>
+    <message>
+        <location filename="../AddEditDialog.py" line="45"/>
+        <source>Edit {{Name}}</source>
+        <translation>Redigera {{Name}}</translation>
+    </message>
+    <message>
+        <location filename="../ui/AddEditDialog.ui" line="0"/>
+        <source>Name:</source>
+        <translation>Namn:</translation>
+    </message>
+    <message>
+        <location filename="../ui/AddEditDialog.ui" line="0"/>
+        <source>Comment:</source>
+        <translation>Kommentar:</translation>
+    </message>
+    <message>
+        <location filename="../ui/AddEditDialog.ui" line="0"/>
+        <source>Icon:</source>
+        <translation>Ikon:</translation>
+    </message>
+    <message>
+        <location filename="../ui/AddEditDialog.ui" line="0"/>
+        <source>Exec:</source>
+        <translation>Kör:</translation>
+    </message>
+</context>
+<context>
+    <name>AddMenuDialog</name>
+    <message>
+        <location filename="../AddMenuDialog.py" line="27"/>
+        <source>Office</source>
+        <translation>Kontor</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="28"/>
+        <source>Utilities</source>
+        <translation>Verktyg</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="29"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="30"/>
+        <source>Development</source>
+        <translation>Utveckling</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="31"/>
+        <source>Graphics</source>
+        <translation>Grafik</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="32"/>
+        <source>Internet</source>
+        <translation>Internet</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="33"/>
+        <source>Multimedia</source>
+        <translation>Multimedia</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="34"/>
+        <source>Games</source>
+        <translation>Spel</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="35"/>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+    <message>
+        <location filename="../AddMenuDialog.py" line="54"/>
+        <source>Other</source>
+        <translation>Övrigt</translation>
+    </message>
+    <message>
+        <location filename="../ui/AddMenuDialog.ui" line="0"/>
+        <source>Add from Menu</source>
+        <translation>Lägg till från menyn</translation>
+    </message>
+    <message>
+        <location filename="../ui/AddMenuDialog.ui" line="0"/>
+        <source>You can add Programs from your Menu to your Autostart</source>
+        <translation>Du kan lägga till program från menyn i autostarten.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.py" line="23"/>
+        <source>Error loading autostart entries</source>
+        <translation>Fel vid inläsning av autostartposter</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.py" line="23"/>
+        <source>One or more autostart entries could not be loaded due to an error. It is possible that these are symbolic links that can cause problems.</source>
+        <translation>En eller flera autostartposter kunde inte läsas in på grund av ett fel. Det kan vara symboliska länkar som orsakar problem.</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>In the List below you see all programs in the Autostart of your Desktop</source>
+        <translation>I listan nedan visas alla program i skrivbordets autostart.</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>You can enable and disable them with the Checkbox</source>
+        <translation>Du kan aktivera och inaktivera dem med kryssrutan.</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>You can only remove Entries, that you have added, not Entries from your System</source>
+        <translation>Du kan bara ta bort poster som du har lagt till, inte poster från systemet.</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Show System Entries</source>
+        <translation>Visa systemposter</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Add from Menu</source>
+        <translation>Lägg till från menyn</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="0"/>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>jdSimpleAutostart</name>
+    <message>
+        <location filename="../__init__.py" line="35"/>
+        <source>No autostart directories found</source>
+        <translation>Inga autostartkataloger hittades</translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="35"/>
+        <source>No autostart directories were found. Maybe your Desktop Environment don&apos;t support the autostart specification.</source>
+        <translation>Inga autostartkataloger hittades. Skrivbordsmiljön kanske inte stöder autostartspecifikationen.</translation>
+    </message>
+</context>
+</TS>
