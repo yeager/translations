1b3f8214f6ea46a5ed38f42b9fa99fc5d96cf279
Daniel Nylander <github@danielnylander.se>
2026-09-16 19:33:45 +0200

    Archive complete Unit3D statistics Swedish localization

---
+++ Unit3D-Stat/stat-sv.php
@@ -0,0 +1,63 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'by-count' => 'Efter antal',
+    'by-data' => 'Efter data',
+    'by-volume' => 'Av volym',
+    'group' => 'Grupp',
+    'groups' => 'Grupper',
+    'nerd-stats' => 'Nördstatistik',
+    'nerd-stats-desc' => 'Vi gillar alla statistik. Här är några mått som vi tycker är viktiga',
+    'registration-date' => 'Registreringsdatum',
+    'request-fulfilled' => 'Förfrågan uppfylld',
+    'request-not-fulfilled' => 'Förfrågan är inte uppfylld',
+    'request-pending-aproval' => 'Förfrågan väntar på godkännande',
+    'select-category' => 'Var god välj en kategori nedan',
+    'site-stats' => 'Webbplatsstatistik',
+    'stats' => 'Statistik',
+    'stats-format' => 'All statistik visas i topp 100-format',
+    'top-bankers' => 'Största bankkonton',
+    'top-bountied' => 'Mest belönade',
+    'top-completed' => 'Flest slutförda',
+    'top-dead' => 'Flest döda',
+    'top-downloaded' => 'Mest nedladdade torrenter',
+    'top-downloaders' => 'Flest nedladdningar',
+    'top-dying' => 'Flest döende',
+    'top-leeched' => 'Mest nedladdade',
+    'top-leechers' => 'Flest nedladdare',
+    'top-seeded' => 'Mest seedade',
+    'top-seeding' => 'Mest seedning',
+    'top-seeders' => 'Flest seedare',
+    'top-seedsize' => 'Störst seedstorlek',
+    'top-seedtime' => 'Längst seedtid',
+    'top-uploaders' => 'Flest uppladdningar',
+    'total-download' => 'Total nedladdning',
+    'total-torrents' => 'Totalt antal torrenter',
+    'total-traffic' => 'Total trafik',
+    'total-upload' => 'Total uppladdning',
+    'users-in-group' => 'Användare i gruppen',
+    'users-per-group' => 'Användare per grupp',
+    'active' => 'Aktiv',
+    'languages' => 'Språk',
+    'all' => 'Alla',
+    'all-time' => 'Hela tiden',
+    'banned' => 'Avstängda',
+    'credited' => 'Tillgodoräknade',
+    'disabled' => 'Inaktiva',
+    'last30days' => 'Senaste 30 dagarna',
+    'last60days' => 'Senaste 60 dagarna',
+    'place' => 'Placering',
+    'pruned' => 'Rensade',
+    'real' => 'Verkliga',
+    'updated' => '(Uppdateras var tionde minut!)',
+];
