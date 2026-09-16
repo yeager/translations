e72ce8f0a201f22538f399497350c6ac61edc1fd
Daniel Nylander <github@danielnylander.se>
2026-09-16 19:27:15 +0200

    Archive complete Unit3D notifications Swedish localization

---
+++ Unit3D-Notifications/notifications-sv.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'date' => 'Datum',
+    'delete' => 'Ta bort',
+    'delete-all' => 'Ta bort alla notiser',
+    'mark-all-read' => 'Markera alla som lästa',
+    'mark-read' => 'Markera som läst',
+    'message' => 'Meddelande',
+    'no-notifications' => 'Inga meddelanden hittades',
+    'notifications' => 'Notiser',
+    'read' => 'Läst',
+    'title' => 'Titel',
+    'already-marked-read' => 'Notisen är redan markerad som läst!',
+    'all-deleted' => 'Alla notiser har tagits bort!',
+    'all-marked-read' => 'Alla notiser har markerats som lästa!',
+    'bon-gifts' => 'Bon-gåvor',
+    'comment-tags' => 'Kommentarstaggar',
+    'deleted' => 'Notisen har tagits bort!',
+    'filter-by-type' => 'Filtrera efter notistyp',
+    'marked-read' => 'Notisen har markerats som läst!',
+    'not-existent' => 'Notisen finns inte!',
+    'post-tags' => 'Inläggstaggar',
+    'post-tips' => 'Inläggstips',
+    'request-approvals' => 'Godkända förfrågningar',
+    'request-bounties' => 'Belöningar för förfrågningar',
+    'request-claims' => 'Anspråk på förfrågningar',
+    'request-fills' => 'Uppfyllda förfrågningar',
+    'request-rejections' => 'Avvisade förfrågningar',
+    'request-unclaims' => 'Återtagna anspråk på förfrågningar',
+    'reseed-requests' => 'Förfrågningar om omsådd',
+    'unfollows' => 'Avföljningar',
+];
