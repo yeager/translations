af0b8366d0b02c3d21ba78b2c674207a489c29b6
Daniel Nylander <github@danielnylander.se>
2026-09-16 19:21:18 +0200

    Archive complete Unit3D email Swedish localization

---
+++ Unit3D-Email/email-sv.php
@@ -0,0 +1,56 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'activate-account' => 'Aktivera konto',
+    'ban-reason' => 'Orsak',
+    'banned-footer' => 'Det är följden av att du inte följt reglerna',
+    'banned-header' => 'Ditt konto har stängts av',
+    'bug-description' => 'Problembeskrivning',
+    'bug-footer' => 'Åtgärda det!',
+    'bug-header' => 'Ny felrapport från',
+    'bug-priority' => 'Prioritet',
+    'bug-title' => 'Felrubrik',
+    'contact-header' => 'Nytt kontaktmeddelande från',
+    'contact-message' => 'Meddelande',
+    'contact-name' => 'Namn',
+    'fail-login-greeting' => 'Inloggningen på ditt konto misslyckades!',
+    'fail-login-line1' => 'En felaktig inloggning upptäcktes för ditt konto.',
+    'fail-login-line2' => 'Den här begäran kom från :ip (:host) vid :time',
+    'fail-login-subject' => 'Meddelande om misslyckad inloggning',
+    'footer-link' => 'Om du har problem med att klicka på knappen :actionText kan du kopiera och klistra in webbadressen nedan i din webbläsare:',
+    'invite-header' => 'Inbjudan till',
+    'invite-invited' => 'Du har blivit inbjuden till',
+    'invite-message' => 'Meddelande',
+    'invite-signup' => 'Registrera dig nu',
+    'newreply-header' => 'Det finns ett nytt svar i ditt ämne',
+    'newreply-message' => 'Meddelande',
+    'newreply-replied' => 'Någon har svarat på ditt ämne',
+    'newreply-view' => 'Visa det nu',
+    'no-email-found' => 'Vi kunde inte hitta den här e-postadressen i vårt system!',
+    'register-code' => 'Klicka på knappen nedan för att slutföra aktiveringen av ditt konto',
+    'register-footer' => 'Om knappen ovan inte fungerar kan du kopiera och klistra in webbadressen i webbläsarens adressfält',
+    'register-header' => 'Hej! Tack för att du anmält dig',
+    'report-comment' => 'Kommentar',
+    'report-email' => 'E-post',
+    'report-header' => 'Hej administratör! En länk har rapporterats på',
+    'report-link' => 'Länk',
+    'report-link-hash' => 'Faktisk länk',
+    'thanks' => 'Tack för att du använder',
+    'unban-footer' => 'Välkommen tillbaka!',
+    'unban-header' => 'Ditt konto har öppnats igen',
+    'unban-reason' => 'Orsak',
+    'username-reminder' => 'Du har nyligen begärt ditt användarnamn för vår app. Ditt användarnamn är',
+    'username-sent' => 'Ditt användarnamn har skickats till din e-postadress!',
+    'disabled-header' => 'Ditt konto har avaktiverats',
+    'pruned-header' => 'Ditt konto har rensats',
+];
