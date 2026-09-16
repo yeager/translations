e7857669d817cabec3cc57aa61f285012b624f4d
Daniel Nylander <github@danielnylander.se>
2026-09-16 19:24:44 +0200

    Archive complete Unit3D private messages Swedish localization

---
+++ Unit3D-PM/pm-sv.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'create' => 'Skapa',
+    'delete' => 'Ta bort',
+    'enter-subject' => 'Ange ämne',
+    'from' => 'Från',
+    'inbox' => 'Inkorg',
+    'mark-all-read' => 'Markera alla meddelanden som läst',
+    'message' => 'Meddelande',
+    'messages' => 'Meddelanden',
+    'new' => 'Nytt meddelande',
+    'outbox' => 'Utkorg',
+    'private' => 'Privat',
+    'read' => 'Läst',
+    'received-at' => 'Mottaget',
+    'refresh' => 'Uppdatera',
+    'reply' => 'Svara',
+    'search' => 'Sök efter ämne',
+    'select' => 'Välj en användare',
+    'send' => 'Skicka PM',
+    'send-to' => 'Skicka privat meddelande till',
+    'sent' => 'Skickat',
+    'sent-at' => 'Skickat den',
+    'subject' => 'Ämne',
+    'to' => 'Till',
+    'unread' => 'Oläst',
+    'empty-inbox' => 'Töm inkorgen',
+    'all-marked-read' => 'Alla dina meddelanden har markerats som lästa!',
+    'delete-success' => 'Det privata meddelandet har tagits bort!',
+    'error' => 'Vad försöker du göra här?',
+    'sent-success' => 'Ditt privata meddelande har skickats!',
+];
